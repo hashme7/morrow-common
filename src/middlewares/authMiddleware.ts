1c6070c5398d1641d576e735558ccc32a5e422f3
hashme7 <hashimkakkaden@gmail.com>
2024-11-24 18:43:57 +0530

    Updates

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -9,7 +9,8 @@ export const authenticate = (
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "No token provided, unauthorized" });
+     res.status(401).json({ message: "No token provided, unauthorized" });
+     return;
   }
 
   const token = authHeader.split(" ")[1];
@@ -17,11 +18,9 @@ export const authenticate = (
   const decoded = JWTService.verifyToken(token);
   if (!decoded) {
     res.status(401).json({ message: "Invalid token, unauthorized" });
-    return ;
   }
   if (decoded) {
     res.status(403).json({ message: `Access denied  ${decoded}` })
-    return;
   }
   next();
 };
