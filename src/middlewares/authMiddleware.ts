6ab12ba8f2a68711e4cc04e90f5cfaf7745779f5
hashme7 <hashimkakkaden@gmail.com>
2024-11-24 18:39:50 +0530

    Updates

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -16,12 +16,12 @@ export const authenticate = (
 
   const decoded = JWTService.verifyToken(token);
   if (!decoded) {
-    return res.status(401).json({ message: "Invalid token, unauthorized" });
+    res.status(401).json({ message: "Invalid token, unauthorized" });
+    return ;
   }
-
   if (decoded) {
-    return res.status(403).json({ message: `Access denied  ${decoded}` });
+    res.status(403).json({ message: `Access denied  ${decoded}` })
+    return;
   }
-
   next();
 };
