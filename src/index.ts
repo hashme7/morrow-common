f2078cf06afd6edd3903df5a24e4c8f0053b12d6
hashme7 <hashimkakkaden@gmail.com>
2024-11-24 16:43:12 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { delay } from './library/delay';
 export { JWTService } from './jwt/jwt';
 export { UserServiceService,UserRequest,UserServiceServer,UserServiceClient,TeamResponse } from './grpc/user';
 import dotenv from 'dotenv'
-export {authenticate} from './middlewares/authMiddleware';
+export  {authenticate} from './middlewares/authMiddleware';
 dotenv.config();
 export const environmentVariables = {
     CLOUDINARY_URL:process.env.CLOUDINARY_URL,
--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,25 +1,27 @@
-import { NextFunction,Request,Response } from 'express';
-import { JWTService } from './../jwt/jwt';
+import { NextFunction, Request, Response } from "express";
+import { JWTService } from "./../jwt/jwt";
 
-export const authenticate = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const authHeader = req.headers.authorization;
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ message: "No token provided, unauthorized" });
-        }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided, unauthorized" });
+  }
 
-        const token = authHeader.split(" ")[1]; 
+  const token = authHeader.split(" ")[1];
 
-        const decoded = JWTService.verifyToken(token);
-        if (!decoded) {
-            return res.status(401).json({ message: "Invalid token, unauthorized" });
-        }
+  const decoded = JWTService.verifyToken(token);
+  if (!decoded) {
+    return res.status(401).json({ message: "Invalid token, unauthorized" });
+  }
 
-        if (decoded) {
-            return res.status(403).json({ message: `Access denied  ${decoded}` });
-        }
+  if (decoded) {
+    return res.status(403).json({ message: `Access denied  ${decoded}` });
+  }
 
-        next(); 
-    };
+  next();
 };
