454dafbe25645bc2da373bf7050fbd68c09b5357
hashim <hashimkakkaden@gmail.com>
2025-02-19 17:37:00 +0530

    modification middleware is created

---
+++ src/middlewares/setParamsMiddleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { JWTService } from "../jwt/jwt";
+
+export const modify = (req: Request, res: Response, next: NextFunction) => {
+  const cookies = req.headers.cookie
+    ? Object.fromEntries(
+        req.headers.cookie.split("; ").map((c) => c.split("="))
+      )
+    : {};
+  const decodedAt = JWTService.verifyToken(cookies.accessToken);
+  if (decodedAt) {
+    req.params.userId = decodedAt.id;
+  }
+  next();
+};
