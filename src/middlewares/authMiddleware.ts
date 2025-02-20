ccfa63e2969c1c748373579714790cb481215496
hashim <hashimkakkaden@gmail.com>
2025-02-20 11:56:32 +0530

    Updates

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -7,28 +7,18 @@ export const authenticate = (
   next: NextFunction
 ) => {
   const accessToken = req.cookies.accessToken;
-  console.log(`
-    
-    access Token :- ${accessToken};
-    
-    
-    `)
   const decodedAT = JWTService.verifyToken(accessToken);
   if (!decodedAT) {
     const refreshToken = req.cookies.refreshToken;
     const decodedRT = JWTService.verifyToken(refreshToken);
     if (decodedRT) {
       req.params.userId = decodedRT.id;
-      console.log("req.params", req.params);
       next();
     }else{
-      console.log("unauthorized........");
       res.status(401).json({message:"unathorised"});
     }
   } else {
-    
     req.params.userId = decodedAT.id;
-    console.log("req.params", req.params);
     next();
   }
 };
--- src/middlewares/setParamsMiddleware.ts
+++ src/middlewares/setParamsMiddleware.ts
@@ -2,10 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { JWTService } from "../jwt/jwt";
 
 export const modify = (req: Request, res: Response, next: NextFunction) => {
-  const cookies = req.cookies.accessToken
-  const decodedAt = JWTService.verifyToken(cookies);
-  if (decodedAt) {
-    req.params.userId = decodedAt.id;
+  const cookies = req.headers.cookie
+    ? Object.fromEntries(
+        req.headers.cookie.split("; ").map((c) => c.split("="))
+      )
+    : {};
+  if (cookies) {
+    console.log("cookies:",cookies)
+    const decodedAt = JWTService.verifyToken(cookies.accessToken);
+    if (decodedAt) {
+      req.params.userId = decodedAt.id;
+    }
+    next();
+  } else {
+    res.status(404).json({ cookies: cookies });
   }
-  next(); 
 };
