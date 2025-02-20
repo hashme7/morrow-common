bf7a38ee243062e0ce9fd9287e5f1a45b4b66b92
hashim <hashimkakkaden@gmail.com>
2025-02-20 12:46:47 +0530

    Updates

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -6,19 +6,24 @@ export const authenticate = (
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = req.cookies.accessToken;
-  const decodedAT = JWTService.verifyToken(accessToken);
-  if (!decodedAT) {
-    const refreshToken = req.cookies.refreshToken;
-    const decodedRT = JWTService.verifyToken(refreshToken);
-    if (decodedRT) {
-      req.params.userId = decodedRT.id;
+  try {
+    const accessToken = req.cookies.accessToken;
+    const decodedAT = JWTService.verifyToken(accessToken);
+    if (!decodedAT) {
+      const refreshToken = req.cookies.refreshToken;
+      const decodedRT = JWTService.verifyToken(refreshToken);
+      if (decodedRT) {
+        req.params.userId = decodedRT.id;
+        next();
+      } else {
+        res.status(401).json({ message: "unathorised" });
+      }
+    } else {
+      req.params.userId = decodedAT.id;
       next();
-    }else{
-      res.status(401).json({message:"unathorised"});
     }
-  } else {
-    req.params.userId = decodedAT.id;
-    next();
+  } catch (error) {
+    res.status(500).json({message:error,})
   }
 };
+
--- src/middlewares/setParamsMiddleware.ts
+++ src/middlewares/setParamsMiddleware.ts
@@ -2,19 +2,24 @@ import { NextFunction, Request, Response } from "express";
 import { JWTService } from "../jwt/jwt";
 
 export const modify = (req: Request, res: Response, next: NextFunction) => {
-  const cookies = req.headers.cookie
-    ? Object.fromEntries(
-        req.headers.cookie.split("; ").map((c) => c.split("="))
-      )
-    : {};
-  if (cookies) {
-    console.log("cookies:",cookies)
-    const decodedAt = JWTService.verifyToken(cookies.accessToken);
-    if (decodedAt) {
-      req.params.userId = decodedAt.id;
+  try {
+    const cookies = req.headers.cookie
+      ? Object.fromEntries(
+          req.headers.cookie.split("; ").map((c) => c.split("="))
+        )
+      : {};
+    if (cookies) {
+      console.log("cookies:", cookies);
+      const decodedAt = JWTService.verifyToken(cookies.accessToken);
+      if (decodedAt) {
+        req.params.userId = decodedAt.id;
+      }
+      next();
+    } else {
+      res.status(404).json({ cookies: cookies });
     }
-    next();
-  } else {
-    res.status(404).json({ cookies: cookies });
+  } catch (error) {
+    res.status(500).json(error);
   }
+  
 };
